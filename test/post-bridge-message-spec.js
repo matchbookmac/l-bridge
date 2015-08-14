7dbd38f768de477fd4f51f3fcf818447949acd5a
Ian MacDonald <ian.macdonald@multco.us>
2015-08-14 13:52:10 -0700

    Failed testing nock with regex

--- test/post-bridge-message-spec.js
+++ test/post-bridge-message-spec.js
@@ -6,8 +6,9 @@ var
 
 describe('postBridgeMessage', function () {
   before(function postingToABridge() {
+    var regEx   = new RegExp("'\\{\"bridge\":\"[\\\\a-z'\\s]+\",\"status\":(false|true),\"timeStamp\":\"[\\w\\s\\:\\-\\(\\)]+\"\\}'", "g");
     var aBridge = nock('http://52.26.186.75:80')
-                    .post('/incoming-snmp')
+                    .post('/incoming-snmp', regEx)
                     .reply(200, "post received")
                   ;
   });
@@ -19,9 +20,20 @@ describe('postBridgeMessage', function () {
       timeStamp:timeStamp
     };
     postBridgeMessage(bridgeMessage, function(res, status){
+console.log(res);
       expect(status).to.equal(200);
       expect(res).to.be.a('string');
       done();
     });
   });
 });
+(function (stuff) {
+  this._ = true;
+  console.log(this._);
+})();
+console.log(this._);
+// '\{"bridge":"[\\a-z'\s]+","status":(false|true),"timeStamp":"[\w\s\:\-\(\)]+"\}'
+
+// '\{ bridge: [\\a-z'\s]+,\\n\s+status: (false|true),\\n\s+timeStamp: [\\\w\s\:\-\(\)']+\\' \}'
+
+regEx   = new RegExp("'\\{ bridge: [\\\\a-z'\\s]+,\\\\n\\s+status: (false|true),\\\\n\\s+timeStamp: [\\\\\\w\\s\\:\\-\\(\\)']+\\\\\' \\}'", "g");
